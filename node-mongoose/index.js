5dad45c04e0c52c1682b346e76d2ae2b497334e8
MusaAliJ <musajprogrammer@gmail.com>
2019-08-29 00:24:54 +0500

    Rest api with mongodb, mongoose

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,19 +8,34 @@ connect.then((db)=>{
 
     console.log("Connect correctly to server");
 
-    var newDish = Dishes({
+    Dishes.create({
         name:"Uthappizza",
         description:"test"
-    });
+    })
 
-    newDish.save()
     .then((dish)=>{
         console.log(dish);
 
-        return Dishes.find({});
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set:{description:"Updated Test"}
+        },{
+            new:true
+        }).exec();
     })
-    .then((dishes)=>{
-        console.log(dishes);
+    .then((dish)=>{
+        console.log(dish);
+        
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish)=>{
+        console.log(dish);
+
         return Dishes.remove({});
     })
     .then(()=>{
