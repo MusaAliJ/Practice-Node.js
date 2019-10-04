ca7d07e1342278ecda0e92e14b83bd7023449a5c
MusaAliJ <musajprogrammer@gmail.com>
2019-10-05 04:59:58 +0500

    passport authentication with token

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,6 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require("passport");
+var authenticate = require("./authenticate");
+var config = require("./config");
 
 
 var indexRouter = require('./routes/index');
@@ -16,7 +21,7 @@ const Dishes = require("./models/dishes");
 const Promotions = require("./models/promotions");
 const Leaders = require("./models/leaders");
 
-const url = "mongodb://localhost:27017/conFusion";
+const url = config.mongoUrl;
 const connect =mongoose.connect(url);
 
 connect.then((db)=>{
@@ -25,7 +30,7 @@ connect.then((db)=>{
 
 
 var app = express();
-
+app.use(passport.initialize());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -33,47 +38,20 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-function auth(req,res,next){
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if(!authHeader){
-    var err = new Error("You are not authenticated");
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    next(err);
-    return;
-  }
-
-  var auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-  var user = auth[0];
-  var pass = auth[1];
-  console.log(user + "user is")
-  console.log(pass + "pass is")
-  if(user == "admin" && pass == "password"){
-    next();
-  }
-  else{
-    var err = new Error("You are not authenticated");
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    next(err);
-  }
-
-}
-
-app.use(auth)
+app.use(cookieParser("12345-67890-09876-54321"));
 
 
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use("/promotions" , promoRouter);
 app.use("/leaders", leaderRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
